77e30e62fed0fbe5b9dd42a674f81926799d043f
Marcela Rojas <vmarcelarojasc22@gmail.com>
2023-09-09 13:50:56 -0500

    convirtiendo_laberinto_de_cadena_a_matriz

--- Videojuego python/TheWitcher/parte6.py
+++ Videojuego python/TheWitcher/parte6.py
@@ -1,13 +1,17 @@
 from typing import List, Tuple
+from functools import reduce
+
+
+def leer_mapa(nombre_archivo: str) -> Tuple[str, List[str]]:
+    with open(nombre_archivo) as archivo:
+        lineas = archivo.readlines()
+    coordenadas = lineas.pop(0).strip().split()
+    return reduce(lambda x, y: x + y, lineas).strip(), coordenadas
 
 
 def convertir_a_matriz(laberinto: str) -> List[List[str]]:
-    # Separa la cadena en filas.
     filas = laberinto.strip().split("\n")
-
-    # Convierte cada fila en una lista de caracteres.
     matriz = [list(fila) for fila in filas]
-
     return matriz
 
 
@@ -15,10 +19,7 @@ import os
 
 
 def mostrar_matriz(matriz: List[List[str]]):
-    # Limpia la pantalla.
     os.system("cls" if os.name == "nt" else "clear")
-
-    # Muestra cada fila de la matriz.
     for fila in matriz:
         print("".join(fila))
 
@@ -26,62 +27,49 @@ def mostrar_matriz(matriz: List[List[str]]):
 def jugar_laberinto(
     matriz: List[List[str]], inicio: Tuple[int, int], final: Tuple[int, int]
 ):
-    # Define las coordenadas iniciales del jugador.
     px, py = inicio
-
-    # Mientras el jugador no llegue al final.
     while (px, py) != final:
-        # Asigna el caracter P a las coordenadas del jugador.
         matriz[py][px] = "P"
-
-        # Muestra la matriz en la pantalla.
         mostrar_matriz(matriz)
-
-        # Lee la tecla presionada por el usuario.
         tecla = input()
-
-        # Verifica si la tecla es una flecha hacia arriba.
         if tecla == "\x1b[A":
             nueva_py = py - 1
             if nueva_py >= 0 and matriz[nueva_py][px] != "#":
                 matriz[py][px] = "."
                 py = nueva_py
                 matriz[py][px] = "P"
-
-        # Verifica si la tecla es una flecha hacia abajo.
         elif tecla == "\x1b[B":
             nueva_py = py + 1
             if nueva_py < len(matriz) and matriz[nueva_py][px] != "#":
                 matriz[py][px] = "."
                 py = nueva_py
                 matriz[py][px] = "P"
-
-        # Verifica si la tecla es una flecha hacia la izquierda.
         elif tecla == "\x1b[D":
             nueva_px = px - 1
             if nueva_px >= 0 and matriz[py][nueva_px] != "#":
                 matriz[py][px] = "."
                 px = nueva_px
                 matriz[py][px] = "P"
-
-        # Verifica si la tecla es una flecha hacia la derecha.
         elif tecla == "\x1b[C":
             nueva_px = px + 1
             if nueva_px < len(matriz[0]) and matriz[py][nueva_px] != "#":
                 matriz[py][px] = "."
                 px = nueva_px
                 matriz[py][px] = "P"
-
-    # Asigna el caracter F a las coordenadas finales.
     matriz[py][px] = "F"
-
-    # Muestra la matriz en la pantalla.
     mostrar_matriz(matriz)
-
     print("¡Felicidades! ¡Has ganado!")
 
 
-laberinto = "###.#.#.#.#.#.#.#.#.#.#.#.#.#.#.#.#.###\n#...#...#...#...#...#...#...#...#...#\n#.###.#.###.###.#.###.###.#.###.###.#\n#.#...#...#...#...#...#...#.#.....#.#\n#.###.###.#.#####.###.###.#.#####.#.#\n#...#.....#.....#.....#...#.......#.#\n###.#.#########.#####.###.##########"
+# Llama a la función leer_mapa() para obtener el laberinto y las coordenadas.
+laberinto, coordenadas = leer_mapa("mapa.txt")
+
+# Convierte el laberinto de cadena a matriz.
+matriz = convertir_a_matriz(laberinto)
+
+# Define las coordenadas iniciales y finales del jugador.
 inicio = (0, 0)
-final = (29, 6)
-jugar_laberinto(convertir_a_matriz(laberinto), inicio, final)
+final = (int(coordenadas[1]) - 1, int(coordenadas[0]) - 1)
+
+# Juega el laberinto.
+jugar_laberinto(matriz, inicio, final)
